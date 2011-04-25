54679b1960682a54cd5d06a1210d5ae265917940
Yasuhisa Yoshida <syou6162@gmail.com>
2011-04-26 07:55:45 +0900

    テストセットパープレキシティ計測できるようにした

--- src/DirichletProcess.hpp
+++ src/DirichletProcess.hpp
@@ -54,16 +54,31 @@ struct second_order {
 class DirichletProcess {
 public:
   DirichletProcess(double beta, double gamma) {
-	beta_ = beta; gamma_ = gamma; K_ = -1;
+	beta_ = beta; gamma_ = gamma; N_ = 0; K_ = -1;
   };
+
   ~DirichletProcess() {
   };
+
   void add(const std::string& word) {
 	N_++;
 	w_.push_back(getID(word, true));
 	z_.push_back(-1); // initialize
   };
-  
+
+  void initialize() {
+	increase(0, 0);
+	for (int i = 1; i < N_; i++) {
+	  // number of existing tables + new table
+	  std::vector<double> posts(get_num_of_clusters() + 1);
+	  double psum = make_cumsum_vector(posts, i);
+	  int next_z = sample_from_multinomial(posts, psum);
+	  increase(i, next_z);
+	}
+	std::cerr << "Number of clusters is " << get_num_of_clusters() << std::endl;
+	std::cerr << "Perplexity: " << perplexity(w_) << std::endl;
+  }
+
   void increase(int index, int cluster) {
 	z_[index] = cluster;
 	if (Nk_.find(cluster) == Nk_.end()) {
@@ -76,7 +91,7 @@ public:
 	if (--(Nkw_[cluster][w_[index]]) == 0) {
 	  Nkw_[cluster].erase(w_[index]);
 	}
-	if (--Nk_[cluster] == 0) {
+	if (--(Nk_[cluster]) == 0) {
 	  Nk_.erase(cluster);
 	  Nkw_.erase(cluster);
 	  shift_cluster_id(cluster);
@@ -104,7 +119,8 @@ public:
 	int next_z = 0;
 	posts[0] = exp(posts[0] - psum);
 	if (r > posts[0]) {
-	  for (int k = 1; k < get_num_of_clusters() + 1; k++) {
+	  //	  for (int k = 1; k < get_num_of_clusters() + 1; k++) {
+	  for (unsigned int k = 1; k < posts.size(); k++) {
 		posts[k] = exp(posts[k] - psum) + posts[k - 1];
 		if (r <= posts[k]) {
 		  next_z = k;
@@ -135,20 +151,8 @@ public:
 	posts[new_cluster_id] -= log(N_ - 1 + gamma_);
 	posts[new_cluster_id] += log(1.0);
 	posts[new_cluster_id] -= log(get_num_of_vocablary());
-	return logsumexp(psum, posts[new_cluster_id], new_cluster_id == 0);
+	return logsumexp(psum, posts[new_cluster_id], false);
   };
-  
-  void initialize() {
-	increase(0, 0);
-	for (int i = 1; i < N_; i++) {
-	  // number of existing tables + new table
-	  std::vector<double> posts(get_num_of_clusters() + 1);
-	  double psum = make_cumsum_vector(posts, i);
-	  int next_z = sample_from_multinomial(posts, psum);
-	  increase(i, next_z);
-	}
-	std::cout << "Number of clusters is " << get_num_of_clusters() << std::endl;
-  }
 
   void gibbs_sampling() {
 	for (int i = 0; i < N_; i++) {
@@ -158,7 +162,8 @@ public:
 	  int next_z = sample_from_multinomial(posts, psum);
 	  increase(i, next_z);
 	}
-	std::cout << "Number of clusters is " << get_num_of_clusters() << std::endl;
+	std::cerr << "Number of clusters is " << get_num_of_clusters() << std::endl;
+	std::cerr << "Perplexity: " << perplexity(w_) << std::endl;
   }
 
   int getID(const std::string& str, const bool train) {
@@ -196,6 +201,18 @@ public:
 	std::cout << "V: " << get_num_of_vocablary() << std::endl;
   };
 
+  void print_state() {
+	for (std::vector<int>::iterator it = z_.begin(); it != z_.end(); it++) {
+	  std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Number of instances in each cluster: ";
+	for (int k = 0; k < get_num_of_clusters(); k++) {
+	  std::cout << Nk_[k] << " ";
+	}
+	std::cout << std::endl;
+  };
+
   int get_num_of_clusters() {
 	return K_ + 1;
   };
@@ -203,15 +220,59 @@ public:
   int get_num_of_vocablary() {
 	return id2word_.size();
   };
+
+  double perplexity(const std::vector<int>& words) {
+	double result = 0.0;
+	for (std::vector<int>::const_iterator it = words.begin(); it != words.end(); it++) {
+	  double sum = 0.0;
+	  for (int k = 0; k < get_num_of_clusters(); k++) {
+		double tmp 
+		  = log(Nkw_[k][*it] + beta_) - log(Nk_[k] + get_num_of_vocablary() * beta_) 
+		  + log(Nk_[k]) - log(N_ + gamma_);
+		sum = logsumexp(sum, tmp, (k == 0));
+	  }
+	  double tmp = log(1.0) - log(get_num_of_vocablary()) 
+		+ log(gamma_) - log(N_ + gamma_); 
+	  sum = logsumexp(sum, tmp, false);
+	  result += sum;
+	}
+	return exp(- result / (double) words.size());
+  };
+
+
+  double log_likelihood() {
+	double result = 0.0;
+	// log(x | z)
+	result += get_num_of_clusters() * lgamma(beta_ * get_num_of_vocablary()) 
+	  - get_num_of_clusters() * get_num_of_vocablary() * lgamma(beta_);
+	for (int w = 0; w < get_num_of_vocablary(); w++) {
+	  for (int k = 0; k < get_num_of_clusters(); k++) {
+		result += lgamma(Nkw_[k][w] + beta_) - lgamma(Nk_[k] + beta_ * get_num_of_vocablary());
+	  }
+	}
+	// log(z)
+	result += get_num_of_clusters() * log(gamma_);
+	for (int k = 0; k < get_num_of_clusters(); k++) {
+	  for (int i = 1; i < Nk_[k]; i++) {
+		result += log(i);
+	  }
+	}
+	for (int i = 1; i <= N_; i++) {
+	  result -= log(gamma_ + i - 1);
+	}
+	return result;
+  };
+
 protected:
   std::map<std::string, int> word2id_;
   std::vector<std::string> id2word_;
+
   std::vector<int> w_; // word
   std::vector<int> z_; // table id or cluster id 
   Rand<boost::uniform_real<> > randomValue_;
 
-  std::map<int, int> Nk_; // Number of customers in table k
-  std::map<int, std::map<int, int> > Nkw_; // Number of customers in table k
+  std::tr1::unordered_map<int, int> Nk_; // Number of customers in table k;
+  std::tr1::unordered_map<int, std::map<int, int> > Nkw_; // Number of customers in table k
 
   int N_; // Number of Data
   int K_; // Number of hidden states
@@ -223,8 +284,7 @@ protected:
   double gamma_;
 
   // ToDo
-  //  double log_likelihood();
-  void sampling();
+  //  void sampling();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,39 @@
 #include "DirichletProcess.hpp"
 
 int main(int argc, char *argv[]) {
-  DirichletProcess dp(0.01, 0.001);
+  DirichletProcess dp(0.001, 0.3);
 
   std::string filename = "tmp.txt";
   std::ifstream ifs(filename.c_str());
 
   std::string line;
+  std::vector<std::string> train;
+  std::vector<int> test;
   while (getline(ifs, line)) {
-	std::vector<std::string> v;
-	boost::algorithm::split(v, line, boost::is_any_of(" .,?!\""));
-	BOOST_FOREACH(std::string s, v) {
-	  dp.add(s);
-	}
+  	std::vector<std::string> v;
+  	boost::algorithm::split(v, line, boost::is_any_of(" .,?!\""));
+  	BOOST_FOREACH(std::string s, v) {
+	  train.push_back(s);
+  	}
   }
 
-  // for (int i = 0; i < 1; i++) {
-  // 	dp.add("0"); dp.add("0"); dp.add("0"); dp.add("0");
-  // 	dp.add("1"); dp.add("1"); dp.add("1"); dp.add("1"); dp.add("1"); dp.add("1"); dp.add("1"); dp.add("1");
-  // 	dp.add("2"); dp.add("2"); dp.add("2");
-  // 	dp.add("3"); dp.add("3"); dp.add("3"); dp.add("3"); dp.add("3"); dp.add("3");
-  // 	dp.add("4"); dp.add("5"); dp.add("6"); dp.add("7"); dp.add("8"); dp.add("9");
-  // }
-  
+  std::random_shuffle(train.begin(), train.end());
+  for (int i = 0; i < 10000; i++){
+	dp.add(train[i]);
+  }
+  for (int i = 10000; i < 13000; i++){
+	test.push_back(dp.getID(train[i], false));
+  }
+
   dp.initialize();
   for (int i = 0; i < 3000; i++) {
 	dp.gibbs_sampling();
+	std::cout << i << "\t" 
+			  << dp.get_num_of_clusters() << "\t" 
+			  << dp.log_likelihood() << "\t" 
+			  << dp.perplexity(test) << std::endl;
   }
+
   dp.print_result();
   return 0;
 };
